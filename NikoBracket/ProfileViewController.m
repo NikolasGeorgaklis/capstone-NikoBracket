6cd146577c2b0e2741cd6f5ac4823115136f5f03
Nikolas Georgaklis <ngeorgak@villanova.edu>
2022-08-02 10:34:19 -0700

    Refine Profile and Edit Profile view contollers
    - Added labels for all modifiable fields in the edit profile view controller
    - Added action sheet for selecting profile picture
    - Added working log out button to edit profile view controller
    - Made profile picture circular in profile and edit profile view controllers
    - Moved all elements up in profile view controller

--- NikoBracket/ProfileViewController.m
+++ NikoBracket/ProfileViewController.m
@@ -24,30 +24,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     self.user = [PFUser currentUser];
-    
     self.pfp.file = self.user[@"profilePicture"];
     self.emailLabel.text = self.user[@"email"];
     self.displayName.text = self.user[@"displayName"];
     self.gradeAndMajor.text = [NSString stringWithFormat:@"%@ %@ Major", self.user[@"grade"], self.user[@"major"]];
+    self.pfp.layer.cornerRadius = self.pfp.frame.size.height/2.0;
 
     [self.pfp loadInBackground];
 }
 
-- (void)updateProfile{
-    self.pfp.file = self.user[@"profilePicture"];
+- (void)updateProfile:(PFImageView *)pfp{
+    self.pfp.file = pfp.file;
+    [self.pfp setImage:pfp.image];
     self.displayName.text = self.user[@"displayName"];
     self.gradeAndMajor.text = [NSString stringWithFormat:@"%@ %@ Major", self.user[@"grade"], self.user[@"major"]];
     self.emailLabel.text = self.user[@"email"];
 
     [self.pfp loadInBackground];
 }
+
 - (IBAction)didLongPressOnPfp:(id)sender {
     UILongPressGestureRecognizer *profileImageExpand = sender;
        CGRect profileImageframe = profileImageExpand.view.frame;
-       profileImageframe.size.height = profileImageExpand.view.frame.size.height * 2;
-       profileImageframe.size.width = profileImageExpand.view.frame.size.width * 2;
+       profileImageframe.size.height = profileImageExpand.view.frame.size.height * 2.5;
+       profileImageframe.size.width = profileImageExpand.view.frame.size.width * 2.5;
        if(profileImageExpand.state == UIGestureRecognizerStateBegan){
            [UIView animateWithDuration:0.3 animations:^{
                profileImageExpand.view.layer.zPosition = MAXFLOAT;
@@ -55,8 +56,8 @@
                profileImageExpand.view.transform = CGAffineTransformMakeTranslation(self.view.center.x - profileImageExpand.view.center.x, self.view.center.y - profileImageExpand.view.center.y);
            } completion:nil];
        }
-       profileImageframe.size.height = profileImageExpand.view.frame.size.height / 2;
-       profileImageframe.size.width = profileImageExpand.view.frame.size.width / 2;
+       profileImageframe.size.height = profileImageExpand.view.frame.size.height / 2.5;
+       profileImageframe.size.width = profileImageExpand.view.frame.size.width / 2.5;
        if(profileImageExpand.state == UIGestureRecognizerStateEnded){
            [UIView animateWithDuration:0.3 animations:^{
                profileImageExpand.view.frame = profileImageframe;
@@ -68,9 +69,7 @@
 
 #pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    //UINavigationController *navVC = [segue destinationViewController];
     if ([segue.identifier isEqualToString:@"editProfileSegue"]) {
         EditProfileViewController *editVC = [segue destinationViewController];
         editVC.delegate = self;
