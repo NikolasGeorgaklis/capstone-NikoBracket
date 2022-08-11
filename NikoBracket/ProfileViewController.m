fc732d8a4e08322a3eb72be366082dece2346d4f
Nikolas Georgaklis <ngeorgak@villanova.edu>
2022-08-11 14:02:34 -0700

    add autolayout to profileviewcontroller
    I also modified the long press gesture function in profileviewcontroller.m because it was distorted by the autolayout constraints.

--- NikoBracket/ProfileViewController.m
+++ NikoBracket/ProfileViewController.m
@@ -78,21 +78,14 @@ static int numOfPastGames;
 
 - (IBAction)didLongPressOnPfp:(id)sender {
     UILongPressGestureRecognizer *profileImageExpand = sender;
-       CGRect profileImageframe = profileImageExpand.view.frame;
-       profileImageframe.size.height = profileImageExpand.view.frame.size.height * 1.7;
-       profileImageframe.size.width = profileImageExpand.view.frame.size.width * 1.7;
+    CGAffineTransform translateToCenter = CGAffineTransformMakeTranslation(self.view.center.x - profileImageExpand.view.center.x, self.view.center.y - profileImageExpand.view.center.y);
        if(profileImageExpand.state == UIGestureRecognizerStateBegan){
            [UIView animateWithDuration:0.3 animations:^{
-               profileImageExpand.view.layer.zPosition = MAXFLOAT;
-               profileImageExpand.view.frame = profileImageframe;
-               profileImageExpand.view.transform = CGAffineTransformMakeTranslation(self.view.center.x - profileImageExpand.view.center.x, self.view.center.y - profileImageExpand.view.center.y);
+               profileImageExpand.view.transform = CGAffineTransformScale(translateToCenter, 2.f, 2.f);
            } completion:nil];
        }
-       profileImageframe.size.height = profileImageExpand.view.frame.size.height / 1.7;
-       profileImageframe.size.width = profileImageExpand.view.frame.size.width / 1.7;
        if(profileImageExpand.state == UIGestureRecognizerStateEnded){
            [UIView animateWithDuration:0.3 animations:^{
-               profileImageExpand.view.frame = profileImageframe;
                profileImageExpand.view.transform = CGAffineTransformMakeTranslation(1.f, 1.f);
            } completion:nil];
        }
